00596182fc077ff4c99af8c3beaf76a8e1ca7994
Godwill <godwillmark@yahoo.co.uk>
2021-02-04 14:17:56 +0200

    eslint disable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
 
 import Tabs from "./components/Tabs";
@@ -55,43 +57,43 @@ class App extends Component {
               <div className="row">
                 <div className="u-content--center">
                   <div className="c-card">
-                    <img className="u-image--rounded" src={obj.avatar} alt="user-image" />
+                    <img className="u-image--rounded" src={obj.avatar} alt="avatar" />
                   </div>
                 </div>
               </div>            
               <div>
                 <Tabs>
-                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/user.svg" alt="icon" />}>
+                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/user.svg" alt="email-icon" />}>
                     <div clasName="row">
                       <h3>Hi, My name is</h3>
                       <h2> {obj.name} </h2>
                     </div>
                   </div>
-                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/email.svg" alt="icon" />}>
+                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/email.svg" alt="email-icon" />}>
                     <div clasName="row">
                       <h3>My email address is</h3>
                       <h2> {obj.email} </h2>
                     </div>
                   </div>                 
-                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/calendar.svg" alt="icon" />}>
+                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/calendar.svg" alt="calendar-icon" />}>
                     <div clasName="row">
                       <h3>My birthday is</h3>
                       <h2> {obj.dob} </h2>
                     </div>
                   </div>
-                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/map-location.svg" alt="icon" />}>
+                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/map-location.svg" alt="location-icon" />}>
                     <div clasName="row">
                       <h3>My address is</h3>
                       <h2> {obj.city} </h2>
                     </div>
                   </div>
-                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/call.svg" alt="icon" />}>
+                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/call.svg" alt="call-icon" />}>
                     <div clasName="row">
                       <h3>My phone number is</h3>
                       <h2> {obj.cell} </h2>
                     </div>
                   </div>
-                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/locked.svg" alt="icon" />}>
+                  <div img={<img className="u-image--24" src="./assets/icons/svg/misc/locked.svg" alt="locked-icon" />}>
                     <div clasName="row">
                       <h3>My password is</h3>
                       <h2> {obj.password} </h2>
